86ac41538f2ec0a5c7bf4e49c4b8a5b95a685b8d
tlkhanh <longkhanhta@gmail.com>
2025-07-21 19:50:22 +0700

    refactor: enhance CoffeesController to include response handling for HTTP status

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,33 +1,33 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
-    constructor(private readonly coffeesService: CoffeesService) { }
-    @Get()
-    getAll() {
-        return this.coffeesService.getAll();
-    }
+  constructor(private readonly coffeesService: CoffeesService) {}
+  @Get()
+  getAll() {
+    return this.coffeesService.getAll();
+  }
 
-    @Post()
-    create(@Body() createCoffeeDto: CreateCoffeeDto) {
-        return this.coffeesService.addCoffee(createCoffeeDto);
-    }
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto, @Res() res: Response) {
+    return res.status(HttpStatus.CREATED).json(this.coffeesService.addCoffee(createCoffeeDto));
+  }
 
-    @Patch(':id')
-    update(@Body() createCoffeeDto: CreateCoffeeDto, @Param('id') id: number) {
-        return this.coffeesService.updateCoffee(id, createCoffeeDto);
-    }
+  @Patch(':id')
+  update(@Body() createCoffeeDto: CreateCoffeeDto, @Param('id') id: number, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.coffeesService.updateCoffee(id, createCoffeeDto));
+  }
 
-    @Get(':id')
-    findById(@Param('id') id: number) {
-        return this.coffeesService.findCoffeeById(id);
-    }
+  @Get(':id')
+  findById(@Param('id') id: number, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.coffeesService.findCoffeeById(id));
+  }
 
-    @Delete(':id')
-    delete(@Param('id') id: number) {
-        return this.coffeesService.deleteCoffee(id);
-    }
-
+  @Delete(':id')
+  delete(@Param('id') id: number, @Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.coffeesService.deleteCoffee(id));
+  }
 }
